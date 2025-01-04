1496d8f7415263e3a10ea4ac9ae630817a7e55ba
Raghav-2104 <mundhararaghav16@gmail.com>
2025-01-04 13:17:00 +0530

    Init Backend

--- Placement Portal/backend/index.js
+++ Placement Portal/backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from 'express';
+import { connectDB } from './db/db.js';
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+connectDB();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});

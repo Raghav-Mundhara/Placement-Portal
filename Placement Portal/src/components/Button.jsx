0463dbd17664676752d585bf9d005b1953e5d6a2
Raghav-2104 <mundhararaghav16@gmail.com>
2025-01-04 11:18:14 +0530

    Setup Folder Structure

---
+++ Placement Portal/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+        <button className='border border-red-900'>
+            Hello
+        </button>
+    </div>
+  )
+}
+
+export default Button
+
---
+++ Placement Portal/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../components/button'
+
+function Home() {
+  return (
+    <div>
+        <Button />
+    </div>
+  )
+}
+
+export default Home

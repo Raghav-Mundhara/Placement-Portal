996605e0c10baa646d6fbade83cc85ab5b6f4417
Raghav-2104 <mundhararaghav16@gmail.com>
2025-01-05 18:56:01 +0530

    Admin Register & Login

--- Placement Portal/backend/index.js
+++ Placement Portal/backend/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { connectDB } from './db/db.js';
 import dotenv from "dotenv";
+import { adminRouter } from './routes/admin.js';
 dotenv.config();
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 connectDB();
 
+app.use('/admin',adminRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
---
+++ Placement Portal/backend/routes/admin.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import {Admin} from '../models/admin.model.js';
+import jwt from 'jsonwebtoken';
+import zod  from 'zod';
+export const adminRouter = express.Router();
+
+const adminLoginSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+});
+
+const adminRegisterSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+});
+
+adminRouter.post('/register', async (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const parseResult = adminRegisterSchema.safeParse(req.body);
+    if(!parseResult.success){
+        return res.status(400).send(parseResult.error);
+    }
+    try{
+        const admin = new Admin({
+            username: username,
+            password: password,
+        });
+        await admin.save();
+        return res.status(201).send(admin);
+    }catch(error){
+        return res.status(400).send(error);
+    }
+});
+
+adminRouter.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const parseResult = adminLoginSchema.safeParse(req.body);
+    if(!parseResult.success){
+        return res.status(400).send(parseResult.error);
+    }
+    try{
+        const admin = await Admin.findOne({
+                username: username,
+                password: password,
+            });
+        if(!admin){
+            res.status(404).send("Admin not found");
+            return;
+        }
+        const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
+        req.headers.authorization = "Token " + token;
+        return res.status(200).send("Token " + token);
+    }catch(error){
+        return res.status(400).send(error);
+    }
+});

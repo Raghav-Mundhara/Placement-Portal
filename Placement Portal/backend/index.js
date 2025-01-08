7042e4cfedb3ca58adf207e917bb7b28f21b5be5
Raghav-2104 <mundhararaghav16@gmail.com>
2025-01-08 15:42:14 +0530

    Student Registeration

--- Placement Portal/backend/index.js
+++ Placement Portal/backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { connectDB } from './db/db.js';
 import dotenv from "dotenv";
 import { adminRouter } from './routes/admin.js';
+import { studentRouter } from './routes/student.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 app.use('/admin',adminRouter);
+app.use('/student',studentRouter);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });

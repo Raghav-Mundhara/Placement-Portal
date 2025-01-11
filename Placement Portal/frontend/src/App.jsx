737537ea82335462088cdbbf9d9a734497fd5519
atharva2125 <atharvaprabhu17@gmail.com>
2025-01-11 15:24:26 +0530

    alumini and footer

--- Placement Portal/frontend/src/App.jsx
+++ Placement Portal/frontend/src/App.jsx
@@ -4,14 +4,16 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
-import LandingPage from './pages/Landingpage'
-import StudentRegister from './pages/StudentRegister'
+import Alumini from './pages/Alumini'
+ 
+ 
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/register" element={<StudentRegister />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Alumini" element={<Alumini />} />
+ 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
--- Placement Portal/frontend/src/pages/Home.jsx
+++ Placement Portal/frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Button from '../components/button'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
     <div className='bg-slate-900'>
-        <Button />
+        <Footer />
+      
     </div>
   )
 }
 
-export default Home
+export default Home;

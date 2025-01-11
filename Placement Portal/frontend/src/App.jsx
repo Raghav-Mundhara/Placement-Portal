d941f3cde80bc1476e77c15e3297820db1c6d88e
vaishnavi <vaishnavi.kumbhar@gmail.com>
2025-01-11 03:46:13 -0800

    Student Verification

--- Placement Portal/frontend/src/App.jsx
+++ Placement Portal/frontend/src/App.jsx
@@ -1,20 +1,24 @@
-
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import AdminDashboard from './pages/AdminDashboard'
 import SignUp from './pages/SignUp'
+
+import StudentVerification from './pages/StudentVerification';
 
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/student-verification/:id" element={<StudentVerification />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
 }
-
 export default App

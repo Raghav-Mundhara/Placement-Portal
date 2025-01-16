4c623c4b4876191204cf783dff191b00be89507c
KanakPendse14 <127370285+KanakPendse14@users.noreply.github.com>
2025-01-16 08:58:01 +0530

    pages include mine and ronak

--- Placement Portal/frontend/src/App.jsx
+++ Placement Portal/frontend/src/App.jsx
@@ -1,17 +1,51 @@
 
+// import './App.css'
+// import {BrowserRouter, Route, Routes} from 'react-router-dom'
+// // import Login from './pages/Login'
+// import Home from './pages/Home'
+// import SignUp from './pages/SignUp'
+
+// function App() {  
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<SignUp />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+// export default App
+
+
+
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-// import Login from './pages/Login'
+import Login from './pages/Login'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
+import Alumini from './pages/Alumini'
+import Registration_1 from './pages/Registration_1'
+import Registration_2 from './pages/Registration_2'
+import AdminDashboard from './pages/AdminDashboard'
+import StudentVerification from './pages/StudentVerification'
+
 
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/Alumini" element={<Alumini />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/" element={<Registration_1 />} /> */}
+        <Route path="/register" element={<Registration_2 />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/student-verification/:id" element={<StudentVerification />} />
+        <Route path='/studentregistration' element={<Registration_1/>}/>
+      
       </Routes>
     </BrowserRouter>
   )
--- Placement Portal/frontend/src/pages/Registration.jsx
+++ Placement Portal/frontend/src/pages/Registration.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-function Registration() {
-  return (
-    <div>
-      Registration
-    </div>
-  )
-}
+// function Registration() {
+//   return (
+//     <div>
+//       Registration
+//     </div>
+//   )
+// }
 
-export default Registration;
+// export default Registration;
